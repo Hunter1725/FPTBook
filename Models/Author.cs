03ce75a22cafdcebfeb8119bbcaae954fdfed2a1
Hunter <98210343+Hunter1725@users.noreply.github.com>
2022-10-06 22:04:14 +0700

    first commit

--- Models/Author.cs
+++
@@ -1,9 +0,0 @@
-namespace FPTBook.Models;
-using System.ComponentModel.DataAnnotations;
-public class Author
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public ICollection<Book>? Books { get; set; }
-}
--- Models/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace FPTBook.Models;
-using System.ComponentModel.DataAnnotations;
-public class Category
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Status { get; set; }
-    public ICollection<Book>? Books { get; set; }
-}
